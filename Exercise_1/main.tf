efd917daa8fd42b2cc0c282afaf87fd954531e39
Nishita Jally <63155407+njally@users.noreply.github.com>
2020-05-25 00:27:56 +0530

    Update main.tf

--- Exercise_1/main.tf
+++ Exercise_1/main.tf
@@ -6,14 +6,14 @@ provider "aws" {
   }
 
 # TODO: provision 4 AWS t2.micro EC2 instances named Udacity T2
-resource "aws_instance" "Udacity T2" {
+resource "aws_instance" "Udacity_T2" {
   count = "4"
   ami = "ami-0470e33cd681b2476"
   instance_type = "t2.micro"
   }
 
 # TODO: provision 2 m4.large EC2 instances named Udacity M4
-resource "aws_instance" "Udacity M4" {
+resource "aws_instance" "Udacity_M4" {
   count = "2"
   ami = "ami-0470e33cd681b2476"
   instance_type = "m4.large"
