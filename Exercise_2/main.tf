319a969f25df4db72d2d6e49f6ba29b0320bb08e
Xander Guzman <xander.guzman@xanderguzman.com>
2020-09-05 10:28:20 -0600

    Exercise 2 work

--- Exercise_2/main.tf
+++ Exercise_2/main.tf
@@ -1,7 +1,7 @@
 # Designate a cloud provider, region, and credentials
 provider "aws" {
-  profile = "sandbox-xander-guzman"
-  region = "us-west-2"
+  region  = var.region
+  profile = var.profile
 }
 
 # Convert the src python file to a zip before uploading it as lambda function.
@@ -79,20 +79,5 @@ resource "aws_iam_role_policy_attachment" "lambda_logs" {
   policy_arn = aws_iam_policy.lambda_logging.arn
 }
 
-# Invoking the Lambda function. Our function doesn't require any input however.
-data "aws_lambda_invocation" "test" {
-  function_name = aws_lambda_function.lambda.function_name
-  input = <<JSON
-{
-  "irrelevant": "sample",
-  "input": "values"
-}
-JSON
-}
 
-# Collecting the output inside a variable which will be printed out during Terraform Apply phase.
-output "Lambda-Execution-Result" {
-  description = "Greeting output"
-  value       = data.aws_lambda_invocation.test.result
-}
 
