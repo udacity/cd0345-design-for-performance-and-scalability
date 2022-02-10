90ba9651eef05a7af008e88907e50554610e2ccd
simeon.aleksandrov@ft.com <simeon.aleksandrov@ft.com>
2022-02-10 16:26:13 +0200

    Destroy m4 instances.

--- Task_5/main.tf
+++ Task_5/main.tf
@@ -14,14 +14,3 @@ resource "aws_instance" "Udacity_T2" {
     Name = "Udacity_T2"
   }
 }
-
-# Provision 2 m4.large EC2 instances named Udacity M4
-resource "aws_instance" "Udacity_M4" {
-  ami           = "ami-033b95fb8079dc481" // Amazon Linux 2
-  instance_type = "m4.large"
-  count         = 2
-  subnet_id     = "subnet-0af813fd84c880467"
-  tags = {
-    Name = "Udacity_M4"
-  }
-}
