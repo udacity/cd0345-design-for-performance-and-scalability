5ec232ff1e96fa5be289f4fab3d4090aba63ca8f
SaberGuirat <saber.guirat@cybeletech.com>
2023-08-17 13:06:40 +0200

    chore: cleanup

--- Exercise_1/main.tf
+++ Exercise_1/main.tf
@@ -1,12 +1,3 @@
-# TODO: Designate a cloud provider, region, and credentials
-
-
-# TODO: provision 4 AWS t2.micro EC2 instances named Udacity T2
-
-
-# TODO: provision 2 m4.large EC2 instances named Udacity M4
-# TODO: Designate a cloud provider, region, and credentials
-
 terraform {
   required_providers {
     aws = {
